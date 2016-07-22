ace244316c4aac131df5a476210c421b1852f563
Oper18 <genovikov93@gmail.com>
2016-07-22 19:04:25 +0300

    Значение на дисплее константа

--- scripts/Main.py
+++ scripts/Main.py
@@ -61,7 +61,8 @@ class MainImpl(object):
             self.c.set("$SEQ.active", "1")
         self.c.report("main.finishTheGameIfDestinationIsFull", "0")
     def setIncreaseSelectionsNbAndDisplayIt(self, key, value):
-        self.selectionsNb = self.selectionsNb + 1
+        #self.selectionsNb = self.selectionsNb + 1
+        self.selectionsNb = 5
         self.displaySelectionsNb()
         self.c.report("main.increaseSelectionsNbAndDisplayIt", "0")
     # replayStartSound.
